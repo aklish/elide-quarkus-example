1f0e6bff12204bd7934f32fae7739d3563bd1f2f
Aaron Klish <klish@verizonmedia.com>
2021-09-13 13:23:36 -0500

    Removed reflection registration from Book

--- src/main/java/example/Book.java
+++ src/main/java/example/Book.java
@@ -2,12 +2,9 @@ package example;
 
 import com.yahoo.elide.annotation.Include;
 
-import io.quarkus.runtime.annotations.RegisterForReflection;
-
 import javax.persistence.Id;
 
 @Include
-@RegisterForReflection
 public class Book {
     @Id
     long id;
